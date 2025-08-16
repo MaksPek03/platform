dbb499950c75e62a8161ddecb13ddfef77a490f2
MaksPek03 <makspek088@gmail.com>
2025-08-16 18:52:28 +0200

    adding log in and register function

--- project/kurs/urls.py
+++ project/kurs/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('register/', views.register_view, name='register'),
+    path('logging', views.logging_view, name = 'logging')
 ]
--- project/kurs/views.py
+++ project/kurs/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 def index(request):
     return render(request, "kurs/index.html")
-    
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request,form.save())
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    return render(request, "kurs/register.html", {"form": form})
+
+def logging_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request,form.get_user())
+            return redirect("index")
+    else: 
+        form = AuthenticationForm()
+    return render(request, 'kurs/logging.html', {"form": form})
